8998ac0bc563b73dc967e19fac066cc76c9f9eec
Mariusz Cichy <mariusz.cichy@mcpm.pl>
2020-10-13 22:21:21 +0200

    Added deploy_dockerhub.sh

---
+++ .circleci/deploy_dockerhub.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+docker login -u $DOCKER_USER -p $DOCKER_PASS
+if [ "$CIRCLE_BRANCH" = "master" ]; then
+    TAG="latest"
+else
+    TAG="$CIRCLE_BRANCH"
+fi
+docker build -f Dockerfile -t $CIRCLE_PROJECT_USERNAME/$CIRCLE_PROJECT_REPONAME:$CIRCLE_TAG .
+docker push $CIRCLE_PROJECT_USERNAME/$CIRCLE_PROJECT_REPONAME:$CIRCLE_TAG
